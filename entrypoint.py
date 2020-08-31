6f15426dc4c7dd8ffc1db3d057db123159003104
Saul Pampa <luaso.1302@gmail.com>
2020-08-31 14:27:21 -0500

    actualizacion

--- entrypoint.py
+++ entrypoint.py
@@ -3,7 +3,7 @@ from app import create_app
 
 
 #settings_module = os.getenv('APP_SETTINGS_MODULE')
-#app = create_app(settings_module)
+#app = create_app(settings_module)flask
 config = DevelopmentConfig
 
 app = create_app(config)
