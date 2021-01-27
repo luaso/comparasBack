9045e37af07d735b9028893d7f25a8c9add73795
Saul <luaso.1302@gmail.com>
2021-01-27 11:54:43 -0500

    arreglado error en comparacion

--- app/usuarioComun/models/crear_Lista_SubastaProductos_model.py
+++ app/usuarioComun/models/crear_Lista_SubastaProductos_model.py
@@ -2,6 +2,7 @@
 from app.db import db, BaseModelMixin
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from datetime import date
 from sqlalchemy import Column, Integer, String, Date
 
 db = SQLAlchemy()
@@ -109,11 +110,13 @@ class Subastas_Productos(db.Model, BaseModelMixin):
 
     @classmethod
     def get_joins_filter_supermercados(self, idSubasta):
+        fechaActual = date.today()
         filtro = db.session.query(Subastas_Productos, Productos_Supermercados, Productos, Supermercados). \
             outerjoin(Productos, Subastas_Productos.idProducto == Productos.idProducto). \
             outerjoin(Productos_Supermercados, Productos.idProducto == Productos_Supermercados.idProducto). \
             outerjoin(Supermercados, Productos_Supermercados.idSupermercado == Supermercados.idSupermercado). \
-            filter(Subastas_Productos.idSubasta == idSubasta).all()
+            filter(Subastas_Productos.idSubasta == idSubasta). \
+            filter(Productos_Supermercados.fechaProducto == fechaActual).all()
         return filtro
 
 
