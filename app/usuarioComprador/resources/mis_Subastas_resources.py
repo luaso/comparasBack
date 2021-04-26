f9431a74e8b6cace44d9843fbfaf0872ebafea13
kuaygong <nelsonwalter1997@gmail.com>
2021-04-26 14:41:32 -0500

    ELIMINAR SUBASTAS V3.1

--- app/usuarioComprador/resources/mis_Subastas_resources.py
+++ app/usuarioComprador/resources/mis_Subastas_resources.py
@@ -48,6 +48,7 @@ class deleteMisSubastasComprador(Resource):
 
             #print("Subastass por eliminar")
             subastas = Subastas.find_by_id(idSubasta)
+            db.session.rollback()
             subastas.delete_from_db()
             result = "completado"
             return {"Result": result}, 200
